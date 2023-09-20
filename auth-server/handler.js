322d919e4b4a505866e13059751eccb32ac29945
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2023-09-20 22:24:21 +0200

    format code

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -35,40 +35,40 @@ module.exports.getAuthURL = async () => {
 };
 
 module.exports.getAccessToken = async (event) => {
-   // Decode authorization code extracted from the URL query
-   const code = decodeURIComponent(`${event.pathParameters.code}`);
+  // Decode authorization code extracted from the URL query
+  const code = decodeURIComponent(`${event.pathParameters.code}`);
 
-return new Promise((resolve, reject) => {
-  /**
-   *  Exchange authorization code for access token with a “callback” after the exchange,
-   *  The callback in this case is an arrow function with the results as parameters: “error” and “response”
-   */
+  return new Promise((resolve, reject) => {
+    /**
+     *  Exchange authorization code for access token with a “callback” after the exchange,
+     *  The callback in this case is an arrow function with the results as parameters: “error” and “response”
+     */
 
-  oAuth2Client.getToken(code, (error, response) => {
-    if (error) {
-      return reject(error);
-    }
-    return resolve(response);
-  });
-})
-  .then((response) => {
-    // Respond with OAuth token 
-    return {
-      statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': true,
-      },
-      body: JSON.stringify(response),
-    };
+    oAuth2Client.getToken(code, (error, response) => {
+      if (error) {
+        return reject(error);
+      }
+      return resolve(response);
+    });
   })
-  .catch((error) => {
-    // Handle error
-    return {
-      statusCode: 500,
-      body: JSON.stringify(error),
-    };
-  });
+    .then((response) => {
+      // Respond with OAuth token 
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+        },
+        body: JSON.stringify(response),
+      };
+    })
+    .catch((error) => {
+      // Handle error
+      return {
+        statusCode: 500,
+        body: JSON.stringify(error),
+      };
+    });
 };
 
 module.exports.getCalendarEvents = async (event) => {
@@ -94,22 +94,22 @@ module.exports.getCalendarEvents = async (event) => {
       }
     );
   })
-  .then((results) => {
-    // Respond with OAuth token 
-    return {
-      statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': true,
-      },
-      body: JSON.stringify({ events: results.data.items }),
-    };
-  })
-  .catch((error) => {
-    // Handle error
-    return {
-      statusCode: 500,
-      body: JSON.stringify(error),
-    };
-  });
+    .then((results) => {
+      // Respond with OAuth token 
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+        },
+        body: JSON.stringify({ events: results.data.items }),
+      };
+    })
+    .catch((error) => {
+      // Handle error
+      return {
+        statusCode: 500,
+        body: JSON.stringify(error),
+      };
+    });
 }
