1e7bd613e11dff208cfe5993eef28807ece9c694
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2023-09-28 15:00:36 +0200

    feat: set up atatus

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('6be9b6986be642568448effea60e61d0').install();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
